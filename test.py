910c2d2872acaabf3e28be7afcb59e46ae2a7c76
toosik <batownikita@gmail.com>
2025-09-21 18:15:48 +0300

    programm for 17 task

--- test.py
+++ test.py
@@ -1,1 +1,19 @@
-print("Bye world!")
+def F(x):
+    return str(x)[0] == str(x)[-1]
+def M(x):
+    return len(str(x)) == 5 and str(x)[1] == "7"
+
+f = open("17.txt")
+a = [int(x) for x in f]
+s = 0
+cnt = 0
+for i in range(len(a) - 2):
+    first = a[i]
+    second = a[i + 1]
+    third = a[i + 2]
+    if sum(F(x) for x in [first, second, third]) == 1:
+        if sum(M(x) for x in [first, second, third]) == 2:
+            cnt += 1
+            s += max(first, second, third)
+
+print(cnt, s)
